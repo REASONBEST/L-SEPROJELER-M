c3a5816fd026b7001e2e6d045186668ace987eb2
REASONBEST <151710432+REASONBEST@users.noreply.github.com>
2024-06-05 13:20:49 +0300

    Add files via upload

---
+++ dizi_foreach/dizi_forinch/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dizi_forinch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] dizi = new string[5];
+            dizi[0] = "Buse";
+            dizi[1] = "Evin";
+            dizi[2] = "Deniz";
+            dizi[3] = "Alper";
+            dizi[4] = "Emirhan";
+
+            foreach (string eleman in dizi)
+            {
+                listBox1.Items.Add(eleman);
+            }
+
+        }
+    }
+}
---
+++ dizi_sayisi_bulma/dizi_sayisi_bulma/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dizi_sayisi_bulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        char[] sesliHarfler = { 'a', 'e', 'u', 'ü', 'ş', 'o', 'ö', 's' };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //METİN GİRİŞİNİ AL
+            string metin = textBox1.Text.ToLower();
+            //harf büyüklüğünü almadan önce metni küçük harfe dönüşütür
+            int sayac = 0;
+            //metnin her harfini kontrol eder
+            for (int i = 0; i < metin.Length; i++)
+            {
+                //kotrol ettiği harfleri sesli olup olmadığına bakar
+                for (int j = 0; j < sesliHarfler.Length; j++)
+                {
+                    if (metin[i] == sesliHarfler[j])
+                    {
+                        sayac++;
+                        break;
+                    }
+                }
+            }
+            label1.Text = "Metindeki sesli harfler: " + sayac;
+        }
+    }
+}
