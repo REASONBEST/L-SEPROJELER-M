d6de6f6d8fbd76e362d8c0d08ff30152a4331d61
REASONBEST <151710432+REASONBEST@users.noreply.github.com>
2024-06-05 13:21:43 +0300

    Add files via upload

---
+++ diziler/diziler/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diziler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[5] { 1, 2, 3, 4, 5 };
+            for (int i = 0; i <= sayilar.Length;i++)
+            {
+                MessageBox.Show(sayilar[i].ToString());
+            }
+        }
+    }
+}
---
+++ diziler_tekrar/diziler_tekrar/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diziler_tekrar
+{
+    public partial class Form1 : Form
+    {
+        char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string metin = textBox1.Text.ToLower();
+
+            int sayac = 0;
+
+            for (int i = 0; i < metin.Length; i++)
+            {
+                for (int j = 0; j < sesliHarfler.Length; j++)
+                {
+                    if (metin[i] == sesliHarfler[j])                                 
+                    {
+                        sayac++;
+                        break;
+                    }
+                }
+            }
+            label1.Text = "Metindeki sesli harf sayısı: " + sayac;
+        }
+    }
+}
