b69041713fe3480d8f0408e78268a1c497e2a5bd
REASONBEST <151710432+REASONBEST@users.noreply.github.com>
2024-06-05 13:58:32 +0300

    Add files via upload

---
+++ Sorumlu/Sorumlu/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorumlu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int not = Convert.ToInt32(textBox1.Text);
+
+            if (not >= 10 && not < 50)
+            {
+                MessageBox.Show("Sorumlu Geçtiniz.");
+            }
+            if (not >= 0 && not < 10)
+            {
+                MessageBox.Show("Kaldınız");
+            }
+            if (not >= 50 && not < 100)
+            {
+                MessageBox.Show("Geçtiniz");
+            }
+        }
+    }
+}
+            
+
+            
+
---
+++ Swith Hesap/Swith Hesap/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swith_Hesap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToByte(textBox1.Text);
+            sayi2 = Convert.ToByte(textBox2.Text);
+            char islem;
+            Convert.ToString(textBox3.Text);
+            switch (islem)
+            {
+                case '+':
+                    sonuc = sayi1 + sayi2;
+                    label4.Text = sonuc.ToString();
+                    break;
+                case '-':
+                    sonuc = sayi1 - sayi2;
+                    label4.Text = sonuc.ToString();
+                    break;
+            }
+               
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
