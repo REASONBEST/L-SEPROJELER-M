d015f370d7ff698ed8c259cc0e71dfbddc5ca1ed
REASONBEST <151710432+REASONBEST@users.noreply.github.com>
2024-06-05 14:02:37 +0300

    Add files via upload

---
+++ Tekli Buton/Tekli Buton/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tekli_Buton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi++;
+            button1.Text = sayi.ToString();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ tekraruygulaması/tekraruygulaması/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekraruygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] dizeler = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60};
+            for (int i = 0; i < dizeler.Length; i++)
+            {
+                if (dizeler[i] % 3 == 0 && dizeler[i] % 5 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+    }
+}
