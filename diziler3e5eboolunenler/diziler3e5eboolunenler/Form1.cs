8cdadfe46e4c8ef1a485928a60f29162836007aa
REASONBEST <151710432+REASONBEST@users.noreply.github.com>
2024-06-05 13:22:55 +0300

    Add files via upload

---
+++ diziler3e5eboolunenler/diziler3e5eboolunenler/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diziler3e5eboolunenler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayi = { 15, 20, 33, 95, 32, 27, 90 };
+            for (int i = 0; i < sayi.Length; i++) 
+            {
+                if (sayi[i] % 5 == 0 && sayi[i] % 3 == 0)
+                {
+                    listBox1.Items.Add(sayi[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ dizilerad/dizilerad/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dizilerad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] ogrenciler = new string[10];
+            
+                ogrenciler[0] = "Miray";
+                ogrenciler[1] = "Hazal";
+                ogrenciler[2] = "Nisa";
+                ogrenciler[3] = "Eylül";
+                ogrenciler[4] = "Deniz";
+                ogrenciler[5] = "Evin Hoca";
+                ogrenciler[6] = "Arda";
+                ogrenciler[7] = "Ali";
+                ogrenciler[8] = "Emirhan";
+                ogrenciler[9] = "Evin Hoca";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                listBox1.Items.Add(ogrenciler[i]);
+            }
+        }
+    }
+}
